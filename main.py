0740a816ed97cfb8c2a0eba90b6bbcf7a5aa250b
Cesar Martinez <cesar.m.g.95@gmail.com>
2025-10-14 17:02:38 +0200

    Actualizaciones ChatOpenAI y primera prueba de llamada a la IA exitosa

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 import os
 from dotenv import load_dotenv
-from langchain_community.llms import OpenAI
+from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
 class Main:
-    def __init__(self):
+    def __init__(
+        self,
+        model_name = "gpt-4o",
+        temperature = 0,
+        max_tokens = 256
+    ):
         api_key = os.getenv("OPENAI_API_KEY")
-        self.llm = OpenAI(openai_api_key=api_key)
+        self.llm = ChatOpenAI(api_key=api_key, model_name=model_name, temperature=temperature, max_tokens=max_tokens)
+
     def test_connection(self):
-        response = self.llm("Hola, ¿puedes responder?")
-        print("Respuesta de Langchain:", response)
+        messages = [
+            (
+                "system",
+                "Eres un asistente útil que busca los mejores regalos",
+            ),
+            ("human", "¿Qué me recomiendas regalar a una persona que le gusta el senderismo?"),
+
+        ]
+        response = self.llm.invoke(messages)
+        print("Respuesta de Langchain:", response.content)
 
 if __name__ == "__main__":
     main = Main()
