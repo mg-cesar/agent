7636d773a8e3cfa5febe2fc4a674450dc03fea00
Cesar Martinez <cesar.m.g.95@gmail.com>
2025-10-14 16:27:30 +0200

    Securización del OpenAI_API_KEY

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from langchain_community.llms import OpenAI
 
 class Main:
     def __init__(self):
-        self.llm = OpenAI(openai_api_key="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")
+        api_key = os.getenv("OPENAI_API_KEY")
+        self.llm = OpenAI(openai_api_key=api_key)
     def test_connection(self):
         response = self.llm("Hola, ¿puedes responder?")
         print("Respuesta de Langchain:", response)
