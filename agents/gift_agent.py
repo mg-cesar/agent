e875696616c52d1d85c259d8ce89f91d71d233d4
Cesar Martinez <cesar.m.g.95@gmail.com>
2025-10-22 11:37:13 +0900

    Añadida la clase persona

--- agents/gift_agent.py
+++ agents/gift_agent.py
@@ -1,12 +1,14 @@
+from models.persona import Persona
+
 class GiftAgent:
     def __init__(self,llm):
         self.llm = llm
     
-    def buscar_regalos(self, aficiones, genero, edad, historico_regalos, presupuesto):
+    def buscar_regalos(self, persona: Persona, presupuesto):
         user_message = (
-            f"Quiero regalar un regalo personalizado a una persona de género {genero}, edad {edad}, "
-            f"con aficionaes: {', '.join(aficiones)}. "
-            f"Ya le he regalado antes: {', '.join(historico_regalos)}. "
+            f"Quiero regalar un regalo personalizado a una persona de género {persona.genero}, edad {persona.edad}, "
+            f"con aficiones: {', '.join(persona.aficiones)}. "
+            f"Ya le he regalado antes: {', '.join(persona.historico_regalos)}. "
             f"Mi presupuesto es de {presupuesto} euros. "
             "Qué regalos me recomiendas?"
         )
